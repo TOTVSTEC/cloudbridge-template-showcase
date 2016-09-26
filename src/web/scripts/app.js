9f0bef0607e43adfc8e5b3ad66ca22104fb8d747
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2016-09-26 16:49:25 -0300

    .

--- src/web/scripts/app.js
+++ src/web/scripts/app.js
@@ -1,5 +1,47 @@
+window.onerror = function(message, source, lineno, colno, error) {
+	var msg = "<div style='color: red'>";
+	msg += '<b>Unhandled Error!</b></br>';
+	msg += 'Message: ' + message + '</br>';
+
+	if (source)
+		msg += 'Source: ' + source + '</br>';
+
+	if (lineno)
+		msg += 'Line: ' + lineno + '</br>';
+
+	if (colno)
+		msg += 'Column: ' + colno + '</br>';
+
+	if (error)
+		msg += 'Error: ' + error + '</br>';
+
+	msg += "</div>";
+
+	log(msg);
+};
+
+function log(text) {
+	var p = document.createElement("p");
+	p.innerHTML = text;
+
+	var elem = document.getElementById('log');
+	if (elem === null) {
+		elem = document.createElement("div");
+		elem.id = 'log';
+		elem.classList.add('log');
+
+		document.body.appendChild(elem);
+	}
+
+	elem.appendChild(p);
+}
+
 var App = function(wsport) {
 	try {
+		$('.splash').remove();
+		$('body').removeClass('disabled');
+		$("#map").modal({show: false});
+
 		this.channel = new TOTVS.TWebChannel(wsport);
 
 		this.on('click', 'device_cam', this.on_device_cam);
@@ -30,256 +72,298 @@ var App = function(wsport) {
 		this.on('click', 'misc_message', this.on_misc_message);
 		this.on('click', 'misc_version', this.on_misc_version);
 		this.on('click', 'misc_ajax', this.on_misc_ajax);
-		
-		document.body.classList.remove('disabled');
-		
-		this.appendText("Started");
+
+
+		log("Started");
 	}
 	catch (ex) {
-		this.appendText("Error");
-		this.appendText(ex);
+		log("Error");
+		log(ex);
 	}
-}
+};
 
 App.prototype.on = function on(event, name, listener) {
-    var elem = document.getElementById(name);
-    elem.addEventListener(event, listener.bind(this));
-}
+	var elem = document.getElementById(name);
+	elem.addEventListener(event, listener.bind(this));
+};
 
 App.prototype.on_device_cam = function on_device_cam(event) {
-    this.channel.getPicture(getPictureSuccess);
-}
+	this.channel.getPicture(function(value) {
+		log('getPicture returned: ' + value);
+	});
+};
 
 App.prototype.on_device_barcode = function on_device_barcode(event) {
-    this.channel.barCodeScanner(barCodeScannerSuccess);
-}
+	this.channel.barCodeScanner(function(value) {
+		log('barCodeScanner returned: ' + value);
+	});
+};
 
 App.prototype.on_device_bt_paired = function on_device_bt_paired(event) {
-    this.channel.pairedDevices(pairedDevicesSuccess);
-}
+	this.channel.pairedDevices(pairedDevicesSuccess);
+};
 
 App.prototype.on_device_geolocation = function on_device_geolocation(event) {
-    this.channel.getCurrentPosition(getCurrentPositionSuccess);
-}
+	this.channel.getCurrentPosition(function(position) {
+		log('getCurrentPosition returned: ' + position);
+
+		if (!position) {
+			position = '23.50623S,46.64419W';
+			//return;
+		}
+
+		// Necessario retirar o sinal de Grau para que o MAPS reconheca a posicao
+		position = position.replace(/?/g, "");
+
+		var map = $("#map");
+
+		map.find('.modal-title').html(position);
+		map.find('iframe')
+			.attr('src', 'http://maps.google.com/?q=' + position + '&output=embed')
+			.css({
+				'height': (window.innerHeight - 120) + 'px'
+				//'width': (window.innerWidth - 40) + 'px'
+			});
+		map.modal('show');
+	});
+};
 
 App.prototype.on_device_orientation_lock = function on_device_orientation_lock(event) {
-    this.channel.lockOrientation();
-}
+	this.channel.lockOrientation();
+};
 
 App.prototype.on_device_orientation_unlock = function on_device_orientation_unlock(event) {
-    this.channel.unlockOrientation();
-}
+	this.channel.unlockOrientation();
+};
 
 App.prototype.on_device_notify = function on_device_notify(event) {
-    this.channel.createNotification(1, "Titulo da Notifica&ccedil;&atilde;o", "Corpo  da Notifica&ccedil;&atilde;o");
-}
+	var options = {
+		id: 1,
+		title: "Titulo da Notifica��o",
+		message: "Corpo da Notifica��o"
+	};
+
+	this.channel.createNotification(options, function(value) {
+		log('createNotification returned: ' + value);
+	});
+};
 
 App.prototype.on_device_vibrate = function on_device_vibrate(event) {
-
-}
+	this.channel.vibrate(1000, function(value) {
+		log('vibrate returned: ' + value);
+	});
+};
 
 App.prototype.on_test_bluetooth = function on_test_bluetooth(event) {
-    this.channel.testDevice(this.channel.BLUETOOTH_FEATURE, testDeviceSuccess);
-}
+	this.channel.testDevice(this.channel.BLUETOOTH_FEATURE, function(value) {
+		log('testDevice BLUETOOTH_FEATURE returned: ' + value);
+	});
+};
 
 App.prototype.on_test_nfc = function on_test_nfc(event) {
-    this.channel.testDevice(this.channel.NFC_FEATURE, testDeviceSuccess);
-}
+	this.channel.testDevice(this.channel.NFC_FEATURE, function(value) {
+		log('testDevice NFC_FEATURE returned: ' + value);
+	});
+};
 
 App.prototype.on_test_wifi = function on_test_wifi(event) {
-    this.channel.testDevice(this.channel.WIFI_FEATURE, testDeviceSuccess);
-}
+	this.channel.testDevice(this.channel.WIFI_FEATURE, function(value) {
+		log('testDevice WIFI_FEATURE returned: ' + value);
+	});
+};
 
 App.prototype.on_test_gps = function on_test_gps(event) {
-    this.channel.testDevice(this.channel.LOCATION_FEATURE, testDeviceSuccess);
-}
+	this.channel.testDevice(this.channel.LOCATION_FEATURE, function(value) {
+		log('testDevice LOCATION_FEATURE returned: ' + value);
+	});
+};
 
 App.prototype.on_test_wifi_conn = function on_test_wifi_conn(event) {
-    this.channel.testDevice(this.channel.CONNECTED_WIFI, testDeviceSuccess)
-}
+	this.channel.testDevice(this.channel.CONNECTED_WIFI, function(value) {
+		log('testDevice CONNECTED_WIFI returned: ' + value);
+	});
+};
 
 App.prototype.on_test_3g_conn = function on_test_3g_conn(event) {
-    this.channel.testDevice(this.channel.CONNECTED_MOBILE, testDeviceSuccess)
-}
+	this.channel.testDevice(this.channel.CONNECTED_MOBILE, function(value) {
+		log('testDevice CONNECTED_MOBILE returned: ' + value);
+	});
+};
 
 App.prototype.on_config_bluetooth = function on_config_bluetooth(event) {
-    this.channel.openSettings(this.channel.BLUETOOTH_FEATURE);
-}
+	this.channel.openSettings(this.channel.BLUETOOTH_FEATURE);
+};
 
 App.prototype.on_config_nfc = function on_config_nfc(event) {
-    this.channel.openSettings(this.channel.NFC_FEATURE);
-}
+	this.channel.openSettings(this.channel.NFC_FEATURE);
+};
 
 App.prototype.on_config_wifi = function on_config_wifi(event) {
-    this.channel.openSettings(this.channel.WIFI_FEATURE);
-}
+	this.channel.openSettings(this.channel.WIFI_FEATURE);
+};
 
 App.prototype.on_config_gps = function on_config_gps(event) {
-    this.channel.openSettings(this.channel.LOCATION_FEATURE);
-}
+	this.channel.openSettings(this.channel.LOCATION_FEATURE);
+};
 
 App.prototype.on_db_create = function on_db_create(event) {
-
-    this.channel.dbExec("create table newTab (cod INTEGER, name TEXT)", dbCreateTableSuccess, dbError);
-}
+	this.channel.dbExec("create table newTab (cod INTEGER, name TEXT)", dbCreateTableSuccess, dbError);
+};
 
 App.prototype.on_db_delete = function on_db_delete(event) {
-    dbDropTable
-}
+	throw new Error('dbDropTable');
+};
 
 App.prototype.on_db_query = function on_db_query(event) {
-    this.channel.dbGet("select * from newTab", dbGetSuccess, dbError);
-}
+	this.channel.dbGet("select * from newTab", dbGetSuccess, dbError);
+};
 
 App.prototype.on_db_tables = function on_db_tables(event) {
-    this.channel.dbGet("SELECT name FROM sqlite_master WHERE type=\"table\"", dbGetSuccess, dbError);
-}
+	this.channel.dbGet("SELECT name FROM sqlite_master WHERE type=\"table\"", dbGetSuccess, dbError);
+};
 
 App.prototype.on_db_rollback = function on_db_rollback(event) {
-    testRollbackProcess
-}
+	//testRollbackProcess
+};
 
 App.prototype.on_db_commit = function on_db_commit(event) {
-    testCommitProcess
-}
+	//testCommitProcess
+};
 
 App.prototype.on_misc_advpl = function on_misc_advpl(event) {
-    this.channel.runAdvpl("DtoS(CtoD(\"" + getDate() + "\"))", runAdvplSuccess);
-}
+	this.channel.runAdvpl("DtoS(CtoD(\"" + getDate() + "\"))", runAdvplSuccess);
+};
 
 App.prototype.on_misc_message = function on_misc_message(event) {
 	this.channel.sendMessage("CLOUDBRIDGE X", function(p1) {
 		var msg = "Return:\n";
 		msg += "type: " + (typeof p1) + "\n";
 		msg += "value: " + p1 + "\n";
-		
+
 		alert(msg);
 	});
-}
+};
 
 App.prototype.on_misc_version = function on_misc_version(event) {
-	console.log("Versão da lib TWebChannel: " + TOTVS.TWebChannel.version);
-		
-    alert("Versão da lib TWebChannel: " + TOTVS.TWebChannel.version);
-}
+	console.log("Vers�o da lib TWebChannel: " + TOTVS.TWebChannel.version);
+
+	alert("Vers�o da lib TWebChannel: " + TOTVS.TWebChannel.version);
+};
 
 App.prototype.on_misc_ajax = function on_misc_ajax(event) {
 	try {
 		$.get("cloudbridge.json", function(data) {
-			$( "body" ).append( data );
-			
-			alert( "success" );
+			$("body").append(data);
+
+			alert("success");
 		})
-		.done(function() {
-			alert( "second success" );
-		})
-		.fail(function() {
-			alert( "error" );
-		})
-		.always(function() {
-			alert( "finished" );
-		});
-		
+			.done(function() {
+				alert("second success");
+			})
+			.fail(function() {
+				alert("error");
+			})
+			.always(function() {
+				alert("finished");
+			});
+
 	}
 	catch (ex) {
-		this.appendText("Error");
-		this.appendText(ex);
+		log("Error");
+		log(ex);
 	}
-}
-
-
-
-App.prototype.appendText = function appendText(text) {
-	var p = document.createElement("p");
-	p.innerHTML = text;
-	
-	document.body.appendChild(p);
-}
-
+};
 
 
 
 function getPictureSuccess(image) {
-    this.channel.createNotification(1, "getPicture", image);
-}
+	this.channel.createNotification(1, "getPicture", image);
+};
 
 function barCodeScannerSuccess(barCode) {
-    this.channel.createNotification(1, "barCodeScanner", barCode);
-}
+	this.channel.createNotification(1, "barCodeScanner", barCode);
+};
 
 function pairedDevicesSuccess(paired) {
-    alert(paired);
-}
+	alert(paired);
+};
 
 function getCurrentPositionSuccess(position) {
-    // Necessario retirar o sinal de Grau para que o MAPS reconheca a posicao
-    position = position.replace(/�/g, "");
-    window.location.href = "http://maps.google.com/?q=" + position;
-}
+	log('');
+
+	if (!position) {
+
+	}
+
+
+	// Necessario retirar o sinal de Grau para que o MAPS reconheca a posicao
+	position = position.replace(/?/g, "");
+	window.location.href = "http://maps.google.com/?q=" + position;
+};
 
 function getDate() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
-    var yyyy = today.getFullYear();
-    return mm + '/' + dd + '/' + yyyy;
-}
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth() + 1; //January is 0!
+	var yyyy = today.getFullYear();
+	return mm + '/' + dd + '/' + yyyy;
+};
 
 function runAdvplSuccess(retStr) {
-    alert(retStr);
-}
+	alert(retStr);
+};
 
 function testDeviceSuccess(lRet) {
-    alert(lRet);
-}
+	alert(lRet);
+};
 
 function dbGetSuccess(data) {
-    var jsonStr = JSON.stringify(data);
-    alert('dbGetSuccess: ' + jsonStr.replace(/"/g, "'"));
-}
+	var jsonStr = JSON.stringify(data);
+	alert('dbGetSuccess: ' + jsonStr.replace(/"/g, "'"));
+};
 
 function dbError(error) {
-    alert("dbError: " + error);
-}
+	alert("dbError: " + error);
+};
 function dbSuccess() {
-    // Dummy
-}
+	// Dummy
+};
 
 // Cria tabela
 function dbCreateTableSuccess() {
-    this.channel.dbExec("insert into newTab values (1,'User 1')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (2,'User 2')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (3,'User 3')", dbSuccess, dbError);
-    alert("dbCreateTableSuccess: Tabela criada com sucesso");
-}
+	this.channel.dbExec("insert into newTab values (1,'User 1')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (2,'User 2')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (3,'User 3')", dbSuccess, dbError);
+	alert("dbCreateTableSuccess: Tabela criada com sucesso");
+};
 function dbCreateTable() {
-    this.channel.dbExec("create table newTab (cod INTEGER, name TEXT)", dbCreateTableSuccess, dbError);
-}
+	this.channel.dbExec("create table newTab (cod INTEGER, name TEXT)", dbCreateTableSuccess, dbError);
+};
 
 // Deleta tabela
 function dbDropTableSuccess() {
-    alert("dbDropTable: Ok");
-}
+	alert("dbDropTable: Ok");
+};
 function dbDropTable() {
-    this.channel.dbExec("drop table newTab", dbDropTableSuccess, dbError);
-}
+	this.channel.dbExec("drop table newTab", dbDropTableSuccess, dbError);
+};
 
 function testCommitProcess() {
-    this.channel.dbBegin(dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (4,'User 4')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (5,'User 5')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (6,'User 6')", dbSuccess, dbError);
-    this.channel.dbCommit(dbSuccess, dbError);
-    alert("testCommitProcess: Ok");
-}
+	this.channel.dbBegin(dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (4,'User 4')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (5,'User 5')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (6,'User 6')", dbSuccess, dbError);
+	this.channel.dbCommit(dbSuccess, dbError);
+	alert("testCommitProcess: Ok");
+};
 
 function testRollbackProcess() {
-    this.channel.dbBegin(dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (4,'User 4')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (5,'User 5')", dbSuccess, dbError);
-    this.channel.dbExec("insert into newTab values (6,'User 6')", dbSuccess, dbError);
-    this.channel.dbRollback(dbSuccess, dbError);
-    alert("testRollbackProcess: Ok");
-}
+	this.channel.dbBegin(dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (4,'User 4')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (5,'User 5')", dbSuccess, dbError);
+	this.channel.dbExec("insert into newTab values (6,'User 6')", dbSuccess, dbError);
+	this.channel.dbRollback(dbSuccess, dbError);
+	alert("testRollbackProcess: Ok");
+};
 
 
